bd7b4d32d32fb05581841f0f6e85c6411a88acd9
Stephen May <SMay@talentpath.com>
2021-07-12 17:55:58 -0400

    filter collection by series

--- CardCollection/Client/card-collection/src/app/app-routing.module.ts
+++ CardCollection/Client/card-collection/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './account/login.component';
 import { CardDetailComponent } from './card-detail/card-detail.component';
 import { CardListComponent } from './card-list/card-list.component';
 import { CollectionByRarityComponent } from './collection-by-rarity/collection-by-rarity.component';
+import { CollectionBySeriesComponent } from './collection-by-series/collection-by-series.component';
 import { CollectionBySuperComponent } from './collection-by-super/collection-by-super.component';
 import { CollectionByTypeComponent } from './collection-by-type/collection-by-type.component';
 import { CollectionListComponent } from './collection-list/collection-list.component';
 import { CollectionViewComponent } from './collection-view/collection-view.component';
 import { SeriesListComponent } from './series-list/series-list.component';
+import { SetTrackerComponent } from './set-tracker/set-tracker.component';
 import { TradeBoardComponent } from './trade-board/trade-board.component';
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
@@ -23,7 +25,8 @@ const routes: Routes = [{path: "cardDetails/:id", component: CardDetailComponent
 {path: "trades", component: TradeBoardComponent},
 {path: "collection/:type", component: CollectionByTypeComponent},
 {path: "collection/super/:super", component: CollectionBySuperComponent},
-{path: "collection/rarity/:rarity", component: CollectionByRarityComponent}]
+{path: "collection/rarity/:rarity", component: CollectionByRarityComponent},
+{path: "collection/series/:series", component: CollectionBySeriesComponent}]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- CardCollection/Client/card-collection/src/app/collection-by-series/collection-by-series.component.ts
+++ CardCollection/Client/card-collection/src/app/collection-by-series/collection-by-series.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { set } from 'src/models/set';
+import { CardService } from 'src/services/card-service';
+import { SeriesListComponent } from '../series-list/series-list.component';
 
 @Component({
   selector: 'app-collection-by-series',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionBySeriesComponent implements OnInit {
 
-  constructor() { }
+  @Input() series!: string;
+  sets!: set[];
+
+  constructor(private service : CardService, private router : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.series = this.router.snapshot.params['series'];
+    this.service.getSetsBySeries(this.series).subscribe(result => {
+      this.sets = result;
+    });
   }
 
 }
